9ff1f7e1b717e25d7b3a5017cafb193d3deb9bb9
ash_joshi1594827151085 <ash_joshi@ymail.com>
2020-08-01 22:25:03 +0530

    Refactored code and removed a extra class.

--- src/main/java/com/ashwin/retail/discount/RetailApplication.java
+++ src/main/java/com/ashwin/retail/discount/RetailApplication.java
@@ -8,7 +8,6 @@ public class RetailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailApplication.class, args);
-
 	}
 
 }
--- src/main/java/com/ashwin/retail/discount/service/DiscountService.java
+++ src/main/java/com/ashwin/retail/discount/service/DiscountService.java
@@ -1,16 +1,37 @@
 package com.ashwin.retail.discount.service;
 
-import com.ashwin.retail.discount.DiscountCalculator;
+import com.ashwin.retail.discount.DiscountSlab;
+import com.ashwin.retail.discount.costumer.CustomerDiscountFactory;
+import com.ashwin.retail.discount.costumer.ICustomerDiscount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DiscountService {
     @Autowired
-    private DiscountCalculator discountCalculator;
+    private CustomerDiscountFactory customerDiscountFactory;
 
     public double calculateDiscountedBill(double bill, String customerType){
-        return bill - discountCalculator.calculateDiscount(bill,customerType);
+        return bill - calculateDiscount(bill,customerType);
     }
 
+    public double calculateDiscount(double bill, String customerType) {
+        double discount = 0;
+        ICustomerDiscount customerDiscount= customerDiscountFactory.getCustomerDiscount(customerType);
+        List<DiscountSlab> discountSlabs = customerDiscount.getDiscountSlabs();
+        for(DiscountSlab discountslab : discountSlabs){
+            if(discountslab.getDiscount() !=0 ) {
+                if (bill > discountslab.getMax()) {
+                    double discountForSlab = discountslab.getSlabAmount() * discountslab.getDiscount() / 100;
+                    discount = discount + discountForSlab;
+                } else if (discountslab.amountInSlab(bill)) {
+                    double discountForSlab = (bill - discountslab.getMin()) * discountslab.getDiscount() / 100;
+                    discount = discount + discountForSlab;
+                }
+            }
+        }
+        return discount;
+    }
 }
